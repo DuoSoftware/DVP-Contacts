68dfe74fef9b258c8233d8c64a7aeb8b65172ec5
sine19 <sineth17@gmail.com>
2018-05-31 18:37:30 +0530

    clear all values
    all default.js values are cleared due to security reasons.

--- config/default.js
+++ config/default.js
@@ -1,20 +1,20 @@
 module.exports = {
     "DB": {
         "Type": "postgres",
-        "User": "duo",
-        "Password": "DuoS123",
+        "User": "",
+        "Password": "",
         "Port": 5432,
         "Host": "localhost",
         "Database": "dvp-engagements"
     },
     "Redis": {
         "mode": "sentinel",//instance, cluster, sentinel
-        "ip": "45.55.142.207",
+        "ip": "",
         "port": 6389,
-        "user": "duo",
-        "password": "DuoS123",
+        "user": "",
+        "password": "",
         "sentinels": {
-            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "hosts": "",
             "port": 16389,
             "name": "redis-cluster"
         }
@@ -23,13 +23,13 @@ module.exports = {
 
     "Security": {
 
-        "ip": "45.55.142.207",
+        "ip": "",
         "port": 6389,
-        "user": "duo",
-        "password": "DuoS123",
+        "user": "",
+        "password": "",
         "mode": "sentinel",//instance, cluster, sentinel
         "sentinels": {
-            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "hosts": "",
             "port": 16389,
             "name": "redis-cluster"
         }
@@ -42,10 +42,10 @@ module.exports = {
         "logfilepath": ""
     },
     "Mongo": {
-        "ip": "104.236.231.11",
+        "ip": "",
         "port": "27017",
-        "dbname": "dvpdb",
-        "password": "DuoS123",
-        "user": "duo"
+        "dbname": "",
+        "password": "",
+        "user": ""
     }
 };
