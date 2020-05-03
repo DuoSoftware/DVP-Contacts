c10baf63eaf6c56c25d593a3347641072f4c52f0
DuoB Coder <duobcoder@gmail.com>
2020-05-03 16:15:03 +0530

    Package update

--- config/default.js
+++ config/default.js
@@ -1,22 +1,22 @@
 module.exports = {
     "DB": {
         "Type": "postgres",
-        "User": "duo",
-        "Password": "DuoS123",
+        "User": "",
+        "Password": "",
         "Port": 5432,
-        "Host": "104.236.231.11",
+        "Host": "",
         "Database": "duo"
     },
     "Redis": {
         "mode": "instance",//instance, cluster, sentinel
-        "ip": "138.197.90.92",
+        "ip": "",
         "port": 6389,
-        "user": "duo",
+        "user": "",
         "db": 0,
-        "password": "DuoS123",
+        "password": "",
         "ttl": 30000,
         "sentinels": {
-            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "hosts": "",
             "port": 16389,
             "name": "redis-cluster"
         }
@@ -25,13 +25,13 @@ module.exports = {
 
     "Security": {
 
-        "ip": "138.197.90.92",
+        "ip": "",
         "port": 6389,
-        "user": "duo",
-        "password": "DuoS123",
+        "user": "",
+        "password": "",
         "mode": "instance",//instance, cluster, sentinel
         "sentinels": {
-            "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+            "hosts": "",
             "port": 16389,
             "name": "redis-cluster"
         }
@@ -45,10 +45,11 @@ module.exports = {
     },
     "Mongo":
         {
-            "ip":"104.236.231.11",
+            "ip":"",
             "port":"27017",
-            "dbname":"dvpdb",
-            "password":"DuoS123",
-            "user":"duo"
+            "dbname":"",
+            "password":"",
+            "user":"",
+            "type": "mongodb+srv",
         },
 };
